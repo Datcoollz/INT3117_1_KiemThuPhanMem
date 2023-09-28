acf14816a196caf6920b6d19dfe322cc90cb9497
Datcoollz <36184579+Datcoollz@users.noreply.github.com>
2023-09-28 21:44:15 +0700

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+
+
+int maxPresent(int n, vi arr){
+    int min = 9;
+    int index = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < min){ 
+            min = arr[i];
+            index = i;
+        }
+    }
+    ++arr[index];
+    int ret = 1;
+    for(int i = 0; i < n; i++){
+        ret *= arr[i];
+    }
+    return ret;
+}
+
+int main() 
+{
+	int n;
+    vi a;
+	cin >> n;
+    for(int i = 0; i < n; i++){
+        int inp;
+        cin >> inp;
+        a.push_back(inp);
+    }
+    cout << maxPresent(n, a);
+    return 0;
+} 
